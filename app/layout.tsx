ce34ca830104dd4e03685e42221cc1bb04b26bda
aaronbanerjee123 <aaronbaner@outlook.com>
2024-09-03 14:29:40 -0400

    implemented navbars and mobile navbar also implemented sheet component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,13 @@ import { IBM_Plex_Sans } from "next/font/google";
 
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({
-  subsets:["latin"], 
-  weight:['400','500','600','700'],
-  variable:'--font-ibm-plex'
-})
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
+});
 
 export const metadata: Metadata = {
   title: "Imaginify",
@@ -23,7 +24,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
+      <ClerkProvider
+        afterSignOutUrl="/"
+        appearance={{
+          variables:{colorPrimary:'#624cf5'},
+          layout: {
+            logoImageUrl:'/assets/images/logo-icon.svg'
+          },
+        }} 
+      >
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
